7c00d264beb0c30f90567b284f72b6177118f230
Cian O'Sullivan <cjp1805@outlook.com>
2021-02-10 21:11:20 +0000

    Initial creation of needed files.
    Adding automl_main file which will run the tests, the imdb dataset and the stopwords files.

---
+++ automl_main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  4 10:54:03 2021
+
+@author: Cian
+"""
+#-------------------------------------------------
+#           ABOUT THIS FILE
+# This file is dedicated to testing the functionality of NLTK
+# 
+# Useful links:
+# https://www.nltk.org/
+# https://scikit-learn.org/stable/modules/feature_extraction.html#text-feature-extraction
+#
+#-------------------------------------------------
+
+
+# To download the prequisites, run this once. 
+def nltk_downloader():
+  import nltk
+  nltk.download('punkt')
+
+def main():
+    # Download prequisites
+    # nltk_downloader()
+
+    
+
+# Run Main
+main()
