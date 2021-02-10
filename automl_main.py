74958a2a3a58c2c5c2aa069827a66094cad50917
Cian O'Sullivan <cjp1805@outlook.com>
2021-02-10 21:36:15 +0000

    Added clean text function

--- automl_main.py
+++ automl_main.py
@@ -20,6 +20,31 @@ def nltk_downloader():
   import nltk
   nltk.download('punkt')
 
+
+def clean_text(text):
+    # Create the lemmatizer
+    wordnet_lemmatizer = WordNetLemmatizer()
+    
+    # Get rid of non alpha characters except "'" as it is needed for the lemment
+    text = "".join(c for c in text if c.isalnum() or c == " " or "'")
+    
+    # Get rid of capitals
+    text = text.lower()
+    
+    # Tokenize the words    
+    # Create tokens of each word
+    token_text = word_tokenize(text)
+    
+    # Get rid of any piece of text that isn't over 2 characters
+    token_text = [t for t in token_text if len(t) > 2] 
+    
+    # Put words in base form by doing lemmatization
+    token_text = [wordnet_lemmatizer.lemmatize(t) for t in token_text]
+    
+    # Return the tokens
+    return token_text
+
+
 def main():
     # Download prequisites
     # nltk_downloader()
