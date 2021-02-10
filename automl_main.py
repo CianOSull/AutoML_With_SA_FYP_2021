1b6c60c8ac37b41da038ca82f8f451c1a6e372c1
Cian O'Sullivan <cjp1805@outlook.com>
2021-02-10 21:37:09 +0000

    Added three functions for calculating TFIDF
    TFIDF = Term Frequency Inverse Document Frequency

--- automl_main.py
+++ automl_main.py
@@ -45,6 +45,54 @@ def clean_text(text):
     return token_text
 
 
+# This function will get the term frequencies for word in the review
+# TF = Term I frequency in document/total words in document
+def calc_tf(term_count, review_corpus):
+    # A dictionary of all the term frequencies found
+    tf_freq = dict.fromkeys(term_count.keys(), 0)   
+    
+    # Review corpus is a tokenized list so the total words iteh length
+    total_words = len(review_corpus)
+    
+    # Calculate the term frequency for each word
+    for word, count in term_count.items():
+        tf_freq[word] = count/total_words
+        
+    return tf_freq
+
+
+# This calcualtes the idf
+# IDF = log(2)*(Total number of Documents/documents frequency or documents with term)
+def calc_idf(unique_terms, list_doc_terms):   
+    # A dicitonary of all the inverse document frequencies
+    idf = dict.fromkeys(unique_terms, 0)
+    
+    # Basically list_doc_terms has all the documents with the term count for each word
+    # You go through each document count the terms where they occured
+    for doc_terms in list_doc_terms:  
+        # This for loop is counting the amount of document a word was in
+        for word, value in doc_terms.items():
+            if 0 < value:
+                idf[word] += 1
+        
+    # Now we calculate idf
+    for word, value in idf.items():
+        idf[word] = math.log10(10 / float(value))
+    
+    return idf
+
+# Modified this function to return a list as dictionaries arn't needed anymore
+def calc_tf_idf(tf, idf, n_terms):
+    # Create an array that is of length of the number of unique terms
+    tf_idf_array = np.zeros(n_terms)
+    
+    for index, (word, value) in enumerate(tf.items()):
+        # Add the tfidf to the array
+        tf_idf_array[index] = value*idf[word]
+    
+    return tf_idf_array
+
+
 def main():
     # Download prequisites
     # nltk_downloader()
